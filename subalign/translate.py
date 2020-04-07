303db54fb7552781c5b477d212f1a32a9392b8b0
ychalier <yohan.chalier@telecom-paristech.fr>
2020-04-07 15:56:02 +0200

    Removed the copy dependency in translate submodule

--- subalign/translate.py
+++ subalign/translate.py
@@ -1,6 +1,5 @@
 """This module provides tools to translate subs from one language to another"""
 
-import copy
 import logging
 import word2word  #pylint: disable=E0401
 
@@ -9,7 +8,7 @@ def translate(sequence, from_language, to_language):
     """Translate a WordSequence from one language to another"""
     logging.info("Translating sequence from '%s' to '%s'", from_language, to_language)
     translator = word2word.Word2word(from_language, to_language)
-    new_sequence = copy.deepcopy(sequence)
+    new_sequence = sequence.offset(0)
     for element in new_sequence:
         try:
             element.word = " ".join(map(lambda s: s.lower(), translator(element.word)))
